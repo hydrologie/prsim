76a0686b59f31ec5a838985c823c8ead6b49ee75
tito_am <fabian.arandia@gmail.com>
2020-07-24 16:44:02 -0400

    Jai enleve les annees avant 1960 pour la simulation selon les indications de leslie

--- 0_lecture_generation_stoch_bassins_kappa_ld_1_1000.R
+++ 0_lecture_generation_stoch_bassins_kappa_ld_1_1000.R
@@ -13,24 +13,29 @@ fichiers<-list.files()
 for(fichier in fichiers){
   
   nom<-strsplit(fichier,'_')[[1]][4]
+  
   date_debut<-strsplit(fichier,'_')[[1]][3]
+  date_debut_sim<-"19600101"
   date_fin<-substr(strsplit(fichier,'_')[[1]][5],1,8)
   
   
   Qobs<-read.delim(fichier,header = FALSE,col.names = 'Qobs')
   Qobs<-Qobs$Qobs
   
-  dates_complete<-seq(as.Date(date_debut,'%Y%m%d'),as.Date("20161231",'%Y%m%d'),by='day')
+  
+  dates_complete<-seq(as.Date(date_debut_sim,'%Y%m%d'),as.Date("20161231",'%Y%m%d'),by='day')
   dates<-seq(as.Date(date_debut,'%Y%m%d'),as.Date(date_fin,'%Y%m%d'),by='day')
   
-  Qobs<-Qobs[dates==dates_complete]
   
-  YYYY<-year(dates[dates==dates_complete])
-  MM<-month(dates[dates==dates_complete])
-  DD<-day(dates[dates==dates_complete])
+  Qobs<-Qobs[which(dates %in% dates_complete)]
+  
+  YYYY<-year(dates[which(dates %in% dates_complete)])
+  MM<-month(dates[which(dates %in% dates_complete)])
+  DD<-day(dates[which(dates %in% dates_complete)])
   
   df<-data.frame(YYYY,MM,DD,Qobs)
   
+
   
   
   tests[[nom]]<-df
