caa0b642eb121ed0bb770b884725debd808759e5
tito_am <fabian.arandia@gmail.com>
2020-08-12 15:52:19 -0400

    Version du code qui permet de comparer les statistiques sommaires a carillon. Faut generaliser le code pour verifier les statistiques par bassin et aussi par aggregation de plusieurs bassins.

--- verif_somme_apports.r
+++ verif_somme_apports.r
@@ -5,12 +5,37 @@
 library(tidyverse)
 library(readxl)
 rm(list=ls())
+
+#historique
+load('/media/tito/TIIGE/PRSIM/obs_outaouais.Rdata')#tests
+
+qobs_total<-list()
+for(bv in names(tests)){
+  qobs_total[[bv]]<-tests[[bv]]$Qobs
+  
+}
+
+qobs_df<-do.call(cbind,qobs_total)#55 annees de donnees
+carillon_sum_obs<-rowSums(qobs_df)
+q_obs_df_final<-as.data.frame(cbind(tests[[bv]]$YYYY,tests[[bv]]$MM,tests[[bv]]$DD,carillon_sum_obs))
+colnames(q_obs_df_final)<-c('YYYY','MM','DD','qobs')
+
+
+qobs_mean_per_day = q_obs_df_final %>% group_by(MM,DD) %>% summarise(AvgQ=mean(qobs))
+qobs_max_per_day = q_obs_df_final %>% group_by(MM,DD) %>% summarise(MaxQ=max(qobs))
+qobs_min_per_day = q_obs_df_final %>% group_by(MM,DD) %>% summarise(MinQ=min(qobs))
+
+#ggplot des statistiques sommaires des observations
+plot(qobs_mean_per_day$AvgQ,type='l',ylim=c(0,20000))                
+points(qobs_max_per_day$MaxQ,type='l')
+points(qobs_min_per_day$MinQ,type='l')
+
 #aller lire les fichiers dans le repertoire pour hecressim
 mainDir<-'/media/tito/TIIGE/PRSIM/0.9995/bv_csv_hecressim'
 setwd(mainDir)
 
 
-total<-list()
+
 
 mainDir2<-'/media/tito/TIIGE/PRSIM/0.9995/'
 subDir<-'carillon_sum'
@@ -64,9 +89,42 @@ df_min_per_julian_day_ordered<-df_min_per_julian_day[order(df_min_per_julian_day
 plot(df_max_per_julian_day_ordered,type='l',ylim=c(0,20000))                
 points(df_mean_per_julian_day_ordered,type='l')
 points(df_min_per_julian_day_ordered,type='l')
+#ajout des informations de debit observes
+points(qobs_mean_per_day$AvgQ,type='l',col='red')                
+points(qobs_max_per_day$MaxQ,type='l',col='red')
+points(qobs_min_per_day$MinQ,type='l',col='red')
 #sdf_pivot(testo, sim_number ~ carillon_sum)
 
+#calcul de la pointe a carillon
+#ajouter les saisons
+#res=testo%>%filter(season %in% target)%>%group_by(sim_number)%>%summarize(max=max(carillon_sum))%>%collect()
 
+#maximun par annee
+res=testo%>%group_by(sim_number)%>%summarize(max=max(carillon_sum))%>%collect()
+#ecdf cunnane
+ecdf_cunnane<-function (x) 
+{
+  x <- sort(x)
+  n <- length(x)
+  if (n < 1) 
+    stop("'x' must have 1 or more non-missing values")
+  vals <- unique(x)
+  rval <- approxfun(vals, cumsum(tabulate(match(x, vals))-0.4)/(n+0.2), 
+                    method = "constant", yleft = 0, yright = 1, f = 0, ties = "ordered")
+  class(rval) <- c("ecdf", "stepfun", class(rval))
+  assign("nobs", n, envir = environment(rval))
+  attr(rval, "call") <- sys.call()
+  rval
+}
+ecdf_max_year<-ecdf_cunnane(res$max)
+plot(ecdf_max_year)
+Fn<- ecdf_cunnane(res$max)
+#prendre les codes pour cunnane
+quantiles_qinter<-data.frame(quantiles=quantile(Fn, prob = c((1-(1/10000)),(1-(1/2000)),(1-(1/1000)),(1-(1/200)),(1-(1/100)),(1-(1/50)),(1-(1/20)),(1-(1/10)),(1-(1/2))), names = FALSE),row.names=c(10000,2000,1000,200,100,50,20,10,2))
+
+quantiles_qinter_2<-data.frame(quantiles=quantile(ecdf_max_year, prob = c((1-(1/10000)),(1-(1/2000)),(1-(1/1000)),(1-(1/200)),(1-(1/100)),(1-(1/50)),(1-(1/20)),(1-(1/10)),(1-(1/2))), names = FALSE),row.names=c(10000,2000,1000,200,100,50,20,10,2))
+
+#
 
 spark_disconnect(sc)
 
