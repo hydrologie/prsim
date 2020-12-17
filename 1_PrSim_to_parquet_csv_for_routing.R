faffbf356767349cdc92f36458581f682fe8a041
tito arandia <fabian.arandia@gmail.com>
2020-12-17 15:39:18 -0500

    Ajustement pour chemin rélatif

--- 1_PrSim_to_parquet_csv_for_routing.R
+++ 1_PrSim_to_parquet_csv_for_routing.R
@@ -11,22 +11,23 @@ library(readr)
 library(stringr)
 options(scipen = 999)
 rm(list=ls())
-#sc <- spark_connect(master = "local")
-setwd('/home/tito/Desktop')
-fichiers<-list.files('/media/tito/TIIGE/PRSIM/0.9997_harm/sims_final/')
+
+
+path<-'/media/tito/TIIGE/PRSIM/0.9997_harm/'
+fichiers<-list.files(paste0(path,'sims_final/'))
 numeros_r<-c("r1","r2","r3","r4","r5","r6","r7","r8","r9","r10")
 
 #Preallouer la matrice avec les annees
-load("/media/tito/TIIGE/PRSIM/0.9997_harm/sims_final/stoch_sim_10_outaouais_Kappa_102_9997_LD.Rdata")
+load(paste0(path,"sims_final/",fichiers[1]))
 pre_test<-stoch_sim['Bark Lake'][[1]]$simulation
 
 i=0
 for(fichier in fichiers){
   try(rm(stoch_sim))
   
-  load(paste('/media/tito/TIIGE/PRSIM/0.9997_harm/sims_final/',fichier,sep=""))
+  load(paste(path,'sims_final/',fichier,sep=""))
   bvs<-names(stoch_sim)
-  mainDir<-'/media/tito/TIIGE/PRSIM/0.9997_harm/bv_csv_hecressim'
+  mainDir<-paste0(path,'bv_csv_hecressim')
   for(numero_r in numeros_r){
     conc_total<-pre_test$YYYY
     
@@ -41,16 +42,10 @@ for(fichier in fichiers){
     for(year in unique(conc_total$conc_total)){
       hydros_per_sim_per_year<-conc_total[conc_total$conc_total==year,]
       i=i+1
-      filename<-paste('//media/tito/TIIGE/PRSIM/0.9997_harm/bv_csv_hecressim/',str_pad(i, 7, pad = "0"),'.csv',sep='')
+      filename<-paste(path,'bv_csv_hecressim/',str_pad(i, 7, pad = "0"),'.csv',sep='')
       hydros_per_sim_per_year <- hydros_per_sim_per_year[,-1] 
       write.csv(x = hydros_per_sim_per_year[complete.cases(hydros_per_sim_per_year),],filename,row.names = FALSE,quote = FALSE)
     }
   }
-    
-    
-    
-
-      
-      
-    }}
+  }}
     
